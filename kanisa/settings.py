f4a914d09ef346f491069fb06798075d10cf6d3d
kinuthiajr <kinuthiajr@gmail.com>
2024-02-10 20:33:44 +0300

    Added environment variables

--- kanisa/settings.py
+++ kanisa/settings.py
@@ -15,12 +15,16 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environments variables 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*nh-_-+1utz2u_d#vn*set-ovg(qhh558nx+&^be^*n4s$#93h'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-*nh-_-+1utz2u_d#vn*set-ovg(qhh558nx+&^be^*n4s$#93h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,11 +91,11 @@ WSGI_APPLICATION = 'kanisa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kanisaos',
-        'USER':'postgres',
-        'PASSWORD':'saltandlight15-13-16',
-        'PORT':'5432',
-        'HOST':'localhost',
+        'NAME': os.getenv('DB_NAME'),
+        'USER':os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'PORT':os.getenv('DB_PORT'),
+        'HOST':os.getenv('DB_HOST'),
         
     }
 }
@@ -147,8 +151,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = 587 
-EMAIL_HOST_USER = 'niajusteve@gmail.com'
-EMAIL_HOST_PASSWORD = 'keroslctrqcmdojr'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
 ACCEPTATION_URL ='http://127.0.0.1:8000/users/'
