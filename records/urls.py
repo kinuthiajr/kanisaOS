08fbb8ef96b8889692b6b433609646a586315f55
kinuthiajr <kinuthiajr@gmail.com>
2024-03-19 15:27:20 +0300

    Added Edit

--- records/urls.py
+++ records/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('workspace/',views.workspace,name='workspace'),
     path('memberform/',views.member,name='memberform'),
     path('memberstable/',views.member_table,name='memberstable'),
+    path('update/<int:record_id>/',views.edit,name='update'),
 ]
--- records/views.py
+++ records/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from . forms import MemberProfileForm,SpouseForm,ChildrenForm
 from . models import MemberProfile,Spouse,Children
 from django.contrib import messages
@@ -13,7 +13,6 @@ def workspace(request):
 # MemberProfile views
 def member(request):
     if request.method == 'POST':
-        print(request.POST)
         member_form = MemberProfileForm(request.POST)
         if member_form.is_valid():
             member_form.save()
@@ -29,5 +28,18 @@ def member(request):
     return render(request,'records/memberform.html',{'member_form':member_form})
 
 def member_table(request):
+    """ Gets all the objects and presents them as a table """
     members = MemberProfile.objects.all()
     return render(request,'records/memberstable.html',{'members':members})
+
+def edit(request,record_id):
+    """" Populates a form with the data from the database to update it """
+    record = get_object_or_404(MemberProfile,pk=record_id)
+    member_form = MemberProfileForm(instance=record)
+    if request.method == 'POST':
+        member_form = MemberProfileForm(request.POST,instance=record)
+        if member_form.is_valid():
+            member_form.save()
+            messages.success(request,'Successfully updated member')
+            return redirect('memberstable')
+    return render(request,'records/update.html',{'record':record,'member_form':member_form})
