99ae8fcd61debdcd146d453630b593da0cebdb19
kinuthiajr <kinuthiajr@gmail.com>
2024-04-19 16:22:37 +0300

    Removed django_browser_reload

--- kanisa/urls.py
+++ kanisa/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('kiongozi.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),#tailwind config
+    #path("__reload__/", include("django_browser_reload.urls")),#tailwind config
     path('accounts/', include('allauth.urls')),#allauth
     path('',include('records.urls')),
 ] 
